cafa7d48a9a00232413e9cb9a25dcd417743b91e
Andrew Koster <andrewkoster95@gmail.com>
2021-08-16 18:39:43 -0400

    fixed issues in models directory

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {}
-Comment.init({
+class Post extends Model {}
+Post.init({
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
