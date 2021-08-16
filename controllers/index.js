0b1c91ba4988e0e57e552840f7e81da26eb76ee7
Andrew Koster <andrewkoster95@gmail.com>
2021-08-16 19:01:45 -0400

    fixed issues in index.js in the controllers directory

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboard-routes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
